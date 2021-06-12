2a3ff15a41cea389bbe81f33b3a761f03290d634
Colin Bartoe <colinbartoe@Colins-MacBook-Pro.local>
2021-06-12 07:38:22 -0400

    adding ttest total

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,8 +15,23 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 #DELIVERABLE 2
 suspension_coil <- read.csv(file="Suspension_Coil.csv", check.names=F)
-total_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI) .groups="keep")
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups="keep")
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+test_summary
 total_summary <- summary(suspension_coil)
 total_summary
 head(suspension_coil)
+
+
+
+
+#DELIVERABLE 3
+t.test(suspension_coil$PSI, mu=1500)
+
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(suspension_coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
+
+
+
 
